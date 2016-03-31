dd3b370ecc79c846358559fb68a11492432cd847
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2016-03-30 22:20:22 -0400

    WIP

--- Package.swift
+++ Package.swift
@@ -25,6 +25,9 @@ let package = Package(
                     
                 ],
                 name: "LinuxBridge",
+                dependencies: [.Target(name: "module")
+                    
+                ],
                 name: "OpenSSL",
                 dependencies: [.Target(name: "osx")
                     
